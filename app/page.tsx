2e014456df9ae56c12fff417a0216e59521028eb
avocado5182 <46830012+avocado5182@users.noreply.github.com>
2026-05-24 14:44:54 -0400

    add supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from "next";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
   title: "Sightreed",
@@ -13,6 +15,19 @@ export const metadata: Metadata = {
 
 import Home from "./client-page";
 
-export default function Page() {
-  return <Home />;
+export default async function Page() {
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data: todos } = await supabase.from("todos").select();
+  return (
+    <>
+      <Home />
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
